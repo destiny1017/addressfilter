c63f55557951226c86a0bc4708a81d047a9e035c
KDH_Notebook <destiny1017@nate.com>
2022-11-27 14:52:42 +0900

    Initial Commit

---
+++ src/main/java/com/example/addressfilter/AddressfilterApplication.java
@@ -0,0 +1,13 @@
+package com.example.addressfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressfilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressfilterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/addressfilter/AddressfilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.addressfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressfilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
